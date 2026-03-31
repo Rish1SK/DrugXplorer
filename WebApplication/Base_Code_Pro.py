716e3a0ef9d6c6662af728fa0786f9492cbad2bb
Jagadesh001ops <jaga170303@gmail.com>
2026-03-30 22:41:19 -0400

    Update Base_Code_Pro.py

--- WebApplication/Base_Code_Pro.py
+++ WebApplication/Base_Code_Pro.py
@@ -19,8 +19,8 @@ import os
 # Suppress warnings
 warnings.filterwarnings("ignore")
 
-os.environ["MISTRAL_API_KEY"] = "Q9Ma0i7PpobPeTURFZK9kWDMT5pBBaHC"
-os.environ["SERPER_API_KEY"] = "Q9Ma0i7PpobPeTURFZK9kWDMT5pBBaHC"
+os.environ["MISTRAL_API_KEY"] = ""
+os.environ["SERPER_API_KEY"] = ""
 
 llm = LLM(
     model="mistral-small-latest",
